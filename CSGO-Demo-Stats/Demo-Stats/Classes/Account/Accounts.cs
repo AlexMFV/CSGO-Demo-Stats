ac2d2ae6f7ef24acf3f47678d71ebebae000163a
AlexMFV <alexmfv1@gmail.com>
2019-06-26 15:27:43 +0100

    Added Account Collection and updated folders

---
+++ CSGO-Demo-Stats/Demo-Stats/Classes/Account/Accounts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Demo_Stats
+{
+    class Accounts : CollectionBase
+    {
+        public void Add (Account acc)
+        {
+            List.Add(acc);
+        }
+
+        public void Remove(Account acc)
+        {
+            List.Remove(acc);
+        }
+
+        public Account this[int index]
+        {
+            get { return (Account)List[index]; }
+            set { List[index] = value; }
+        }
+    }
+}
